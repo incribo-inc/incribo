526416fd160bcca1b71692d64725b37946ac8c05
uma-08 <43182226+uma-08@users.noreply.github.com>
2024-08-28 19:05:47 +0530

    Create __init__.py

---
+++ src/incribo/__init__.py
@@ -0,0 +1,6 @@
+from .embedding import Embedding, EmbeddingComparator
+from .version_control import VersionControlWrapper, BranchManagerWrapper
+from .drift_detection import DriftManagerWrapper
+from .utils import EvaluationWeights
+
+__version__ = "0.1.2"  
